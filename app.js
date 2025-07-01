a38816e4e80f72b230dc44bd8f41499060b33c33
AH LT <ahussain93@live.co.uk>
2025-07-01 17:01:59 +0100

    Updates

--- app.js
+++ app.js
@@ -73,7 +73,6 @@ passport.deserializeUser(async (id, done) => {
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -83,15 +82,12 @@ app.use((req, res, next) => {
   next();
 })
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({message: 'error'});
 });
 
 app.listen(3000, () =>
